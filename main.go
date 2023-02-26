23a86d433235344360cd8ca051fc296f1f8fa52c
lumino <luminovoez@gmail.com>
2023-02-27 02:28:35 +0800

    maybe later.

--- main.go
+++ main.go
@@ -10,22 +10,12 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Use("/", func(c *fiber.Ctx) error {
-		// IsWebSocketUpgrade returns true if the client
-		// requested upgrade to the WebSocket protocol.
-		if websocket.IsWebSocketUpgrade(c) {
-			c.Locals("allowed", true)
-			return c.Next()
-		}
-		return c.Next()
-	})
-
-	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
+	wsHandler := websocket.New(func(c *websocket.Conn) {
 		// c.Locals is added to the *websocket.Conn
-		log.Println(c.Locals("allowed"))  // true
-		log.Println(c.Params("id"))       // 123
-		log.Println(c.Query("v"))         // 1.0
-		log.Println(c.Cookies("session")) // ""
+		// log.Println(c.Locals("allowed"))  // true
+		// log.Println(c.Params("id"))       // 123
+		// log.Println(c.Query("v"))         // 1.0
+		// log.Println(c.Cookies("session")) // ""
 
 		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
 		var (
@@ -45,8 +35,33 @@ func main() {
 				break
 			}
 		}
+	})
 
-	}))
+	// the order is determined by the order of the sentences.
+	app.Use("/test/2", func(c *fiber.Ctx) error {
+		log.Println("test/2")
+		return c.Next()
+	})
+
+	app.Use("/test/", func(c *fiber.Ctx) error {
+		log.Println("test")
+		return c.Next()
+	})
+
+	app.Use("/", func(c *fiber.Ctx) error {
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return wsHandler(c)
+		}
+		return c.Next()
+	})
+
+	app.All("/*", func(c *fiber.Ctx) error {
+		c.WriteString("...")
+		return nil
+	})
 
 	log.Fatal(app.Listen(":3000"))
 	// Access the websocket server: ws://localhost:3000/ws/123?v=1.0
